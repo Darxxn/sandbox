32a60f95dd4c04df99625dc745a81255c62598a4
Darxxn <stonelai11@gmail.com>
2025-03-17 20:03:10 -0600

    vm for login

--- opsync/lib/viewmodels/register_vm.dart
+++ opsync/lib/viewmodels/register_vm.dart
@@ -5,16 +5,19 @@ class RegisterViewModel extends ChangeNotifier {
   final TextEditingController orgCodeController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-  final TextEditingController confirmPasswordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
 
   void register(BuildContext context) {
-  if (emailController.text.isNotEmpty && passwordController.text.isNotEmpty) {
-    Navigator.pushReplacementNamed(context, '/chat'); // Navigate to Chat Screen
-  } else {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text("Please fill in all fields")),
-    );
+    if (emailController.text.isNotEmpty && passwordController.text.isNotEmpty) {
+      Navigator.pushReplacementNamed(
+        context,
+        '/chat',
+      ); // Navigate to Chat Screen
+    } else {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("Please fill in all fields")));
+    }
   }
-}
-
 }
